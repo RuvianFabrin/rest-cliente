d2a2767ce900b86ef444aeee83e85298294e1ff2
Ruvian Fabrin <ruvian.fabrin@hotmail.com>
2023-03-06 15:48:18 -0300

    Preparado para produção

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
 INSERT INTO tb_cliente (name, created_at) VALUES ('Lucas', now());
 INSERT INTO tb_cliente (name, created_at) VALUES ('Luiza', now());
 INSERT INTO tb_cliente (name, created_at) VALUES ('Luciana', now());
-INSERT INTO tb_cliente (name, created_at) VALUES ('Luciana', now());
+INSERT INTO tb_cliente (name, created_at) VALUES ('Patrik', now());
